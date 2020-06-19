2c923eb5427b5dea00a72fd819f0d4a2e6592066
Caia Chuang <caiachung@gmail.com>
2020-06-19 06:01:20 +0000

    Trent is teaching me

---
+++ conditional-challenge.js
@@ -0,0 +1,11 @@
+const whichSchool = function(age) {
+	if (age < 13){
+		return "Elementary School";
+	} else if (age >= 13 && age <= 18) {
+		return "Secondary School";
+	} else if(age >19 && age < 100){
+		return "Lighthouse Lab";
+	}
+}
+
+console.log(whichSchool(14));
